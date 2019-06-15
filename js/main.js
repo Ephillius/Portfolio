8901befbce08f90edecbba65cc9862a72492d121
Bernardo Ferreira <bernardoferreira12@hotmail.com>
2019-06-15 06:54:31 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,13 +57,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		]
 	];
 
-	const captions = [
-		'A nice and simple two page website that contains my personal info, my online resume (not the extended one) and a simple list of future goals.',
-		'This is a responsive portfolio layout website. It can showcase projects with descriptive information, has a header section that can include a profile photo and a brief summary and a contact (how to reach me) section with basic info. All done with floated elements.',
-		'Simple newsletter layout. Both the Full name and Email Address are required in order to submit the form. Almost every input field has a :focus state. The website was also made responsive with a mobile-first approach.',
-		'Sample Style guide page. Work done with Sass. It was a huge leap from writting Css to start breaking the styling code into pieces. I tried my best to not write in CSS first as that was the initial challenge: to jump straight into Sass. The webpage is totally responsive.',
-		'This is an "interactive" image gallery. I used a Lightbox plugin in order to showcase the pictures in a larger size. For the input (search bar), I had to come up with a search function that would show in realtime the potential matches to what the user was typing at the time, making the non-matched pics disappear.'
-	];
+	// const captions = [
+	// 	'A nice and simple two page website that contains my personal info, my online resume (not the extended one) and a simple list of future goals.',
+	// 	'This is a responsive portfolio layout website. It can showcase projects with descriptive information, has a header section that can include a profile photo and a brief summary and a contact (how to reach me) section with basic info. All done with floated elements.',
+	// 	'Simple newsletter layout. Both the Full name and Email Address are required in order to submit the form. Almost every input field has a :focus state. The website was also made responsive with a mobile-first approach.',
+	// 	'Sample Style guide page. Work done with Sass. It was a huge leap from writting Css to start breaking the styling code into pieces. I tried my best to not write in CSS first as that was the initial challenge: to jump straight into Sass. The webpage is totally responsive.',
+	// 	'This is an "interactive" image gallery. I used a Lightbox plugin in order to showcase the pictures in a larger size. For the input (search bar), I had to come up with a search function that would show in realtime the potential matches to what the user was typing at the time, making the non-matched pics disappear.'
+	// ];
 
 	const portfolio = document.querySelector('#work');
 	const email = document.getElementsByClassName('email');
