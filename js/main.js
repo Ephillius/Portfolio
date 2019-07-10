e80936a3f4d1845ec35c8b651900586e4ad7d3c0
Bernardo Ferreira <bernardoferreira12@hotmail.com>
2019-07-10 16:18:17 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			'images/projects/project6.png',
 			'https://ephillius.github.io/techdegree-project6/',
 			'https://github.com/Ephillius/techdegree-project6'
-		]
+		],
 
 		//7
 		[
@@ -74,13 +74,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		]
 	];
 
-	// const captions = [
-	// 	'A nice and simple two page website that contains my personal info, my online resume (not the extended one) and a simple list of future goals.',
-	// 	'This is a responsive portfolio layout website. It can showcase projects with descriptive information, has a header section that can include a profile photo and a brief summary and a contact (how to reach me) section with basic info. All done with floated elements.',
-	// 	'Simple newsletter layout. Both the Full name and Email Address are required in order to submit the form. Almost every input field has a :focus state. The website was also made responsive with a mobile-first approach.',
-	// 	'Sample Style guide page. Work done with Sass. It was a huge leap from writting Css to start breaking the styling code into pieces. I tried my best to not write in CSS first as that was the initial challenge: to jump straight into Sass. The webpage is totally responsive.',
-	// 	'This is an "interactive" image gallery. I used a Lightbox plugin in order to showcase the pictures in a larger size. For the input (search bar), I had to come up with a search function that would show in realtime the potential matches to what the user was typing at the time, making the non-matched pics disappear.'
-	// ];
 
 	const scrollToWork = document.querySelector('.toWork');
 	const scrollToTop = document.querySelector('.btt');
